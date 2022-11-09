08107c68d2fd3bd811a16a48caf1730906aaeda5
tolumoyin <tolumoyin2002@gmail.com>
2022-11-08 21:04:26 -0700

    checkov recommendations done

--- var.tf
+++ var.tf
@@ -1,11 +1,11 @@
 variable "AMI_ID" {
-    type = string
-    description = "AMI ID"
-    default = "ami-09d3b3274b6c5d4aa"
+  type        = string
+  description = "AMI ID"
+  default     = "ami-09d3b3274b6c5d4aa"
 }
 
 variable "INSTANCE_TYPE" {
-    type = string
-    description = "type of EC2 instance"
-    default = "t3.micro"
+  type        = string
+  description = "type of EC2 instance"
+  default     = "t3.micro"
 }
