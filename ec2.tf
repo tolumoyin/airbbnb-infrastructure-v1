4b5c5ad1eec6902cad5643e0bce3714a4475f3cc
tolumoyin <tolumoyin2002@gmail.com>
2022-11-08 20:19:15 -0700

    added webserver

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_instance" "web" {
+  ami           = var.AMI_ID
+  instance_type = var.INSTANCE_TYPE
+
+  tags = {
+    Name = "Webserver"
+  }
+}
